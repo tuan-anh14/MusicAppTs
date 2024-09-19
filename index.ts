496f2a2c18540b9714cd0ba8ec1924f320308485
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-19 10:52:11 +0700

    Kết nối database

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import express, { Express, Response, Request } from "express";
+import * as database from "./config/database"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+database.connect();
 
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
