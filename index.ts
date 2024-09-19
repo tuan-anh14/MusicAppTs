2a704a05cea184d45cba6840f093f6bf82c0c71d
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-19 14:39:13 +0700

    Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Response, Request } from "express";
-import * as database from "./config/database"
+import * as database from "./config/database";
 import dotenv from "dotenv";
+import Topic from "./models/topic.model";
 
 dotenv.config();
 
@@ -12,7 +13,12 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false,
+  });
+
+  console.log(topics);
   res.render("client/pages/topics/index.pug");
 });
 
