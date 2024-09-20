7e9fcf7093b543e50c21330ee93f3fde6c9195d6
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-20 09:19:05 +0700

    Nhúng file tĩnh

--- index.ts
+++ index.ts
@@ -11,6 +11,8 @@ database.connect();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
