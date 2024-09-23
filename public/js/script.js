6133c55e96c3d170fba8784dd4593d5b507c9e31
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-23 09:22:18 +0700

    Làm trang bài hát yêu thích

--- public/js/script.js
+++ public/js/script.js
@@ -67,27 +67,29 @@ if (buttonLike) {
 // End Button Like
 
 // Button Favorite
-const buttonFavorite = document.querySelector("[button-favorite]");
-if (buttonFavorite) {
-  buttonFavorite.addEventListener("click", () => {
-    const idSong = buttonFavorite.getAttribute("button-favorite");
-    const isActive = buttonFavorite.classList.contains("active");
+const listButtonFavorite = document.querySelectorAll("[button-favorite]");
+if (listButtonFavorite.length > 0) {
+  listButtonFavorite.forEach((buttonFavorite) => {
+    buttonFavorite.addEventListener("click", () => {
+      const idSong = buttonFavorite.getAttribute("button-favorite");
+      const isActive = buttonFavorite.classList.contains("active");
 
-    const typeFavorite = isActive ? "unfavorite" : "favorite";
+      const typeFavorite = isActive ? "unfavorite" : "favorite";
 
-    const link = `/songs/favorite/${typeFavorite}/${idSong}`;
+      const link = `/songs/favorite/${typeFavorite}/${idSong}`;
 
-    const option = {
-      method: "PATCH",
-    };
+      const option = {
+        method: "PATCH",
+      };
 
-    fetch(link, option)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.code == 200) {
-          buttonFavorite.classList.toggle("active");
-        }
-      });
+      fetch(link, option)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.code == 200) {
+            buttonFavorite.classList.toggle("active");
+          }
+        });
+    });
   });
 }
 
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -4,11 +4,15 @@ import { topicRoutes } from "./topic.route";
 
 import { songRoutes } from "./song.route";
 
+import { favoriteSong } from "./favorite-song.route"
+
 const clientRoutes = (app: Express): void => {
 
   app.use(`/topics`, topicRoutes);
 
   app.use(`/songs`, songRoutes);
+
+  app.use(`/favorite-songs`, favoriteSong);
 
 };
 
