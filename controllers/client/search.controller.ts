8fa5795c73053e4dd2b3cfaa264bbecc2e57810b
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-24 22:12:45 +0700

    Tìm kiếm hiển thị suggest ( gợi ý )

--- controllers/client/search.controller.ts
+++ controllers/client/search.controller.ts
@@ -3,8 +3,9 @@ import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
 import { convertToSlug } from "../../helpers/convertToSlug";
 
-// [GET] /search/result
+// [GET] /search/:type
 export const result = async (req: Request, res: Response) => {
+  const type = req.params.type;
   const keyword: string = `${req.query.keyword}`;
 
   let newSongs: any = [];
@@ -25,15 +26,40 @@ export const result = async (req: Request, res: Response) => {
         _id: item.singerId,
       });
 
-      (item as any).infoSinger = infoSinger;
+      // (item as any).infoSinger = infoSinger;
+      newSongs.push({
+        id:item.id,
+        title: item.title,
+        avatar: item.avatar,
+        like: item.like,
+        slug: item.slug,
+        infoSinger: {
+          fullName: infoSinger?.fullName
+        }
+      })
     }
 
-    newSongs = songs;
+    // newSongs = songs;
   }
 
-  res.render("client/pages/search/result.pug", {
-    pageTitle: `Kết quả: ${keyword}`,
-    keyword: keyword,
-    songs: newSongs,
-  });
+  switch (type) {
+    case "result":
+      res.render("client/pages/search/result.pug", {
+        pageTitle: `Kết quả: ${keyword}`,
+        keyword: keyword,
+        songs: newSongs,
+      });
+      break;
+    case "suggest":
+      res.json({
+        code: 200,
+        message: "Thành công!",
+        songs: newSongs,
+      });
+
+      break;
+
+    default:
+      break;
+  }
 };
--- routes/client/search.route.ts
+++ routes/client/search.route.ts
@@ -4,6 +4,6 @@ import * as controller from "../../controllers/client/search.controller";
 
 const router: Router = Router();
 
-router.get("/result", controller.result);
+router.get("/:type", controller.result);
 
 export const searchRoutes: Router = router;
